240cd405b3714fa5754caaeb40e73383381aff8e
ppf <peipengfei@iie.ac.cn>
2022-03-14 19:20:05 +0800

    optimize code

--- layer/helper.py
+++ layer/helper.py
@@ -40,7 +40,7 @@ def tensor2img(image, path=None):
 
 
 def get_hash_json(hash_bit=get_hash_bits()):
-    return str(hash_bit) + '_hash1.json'
+    return str(hash_bit) + '_hash2.json'
 
 
 def gen_hash(hashset: []):
